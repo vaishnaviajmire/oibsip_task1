b8c2490bbd83dd63a7123f14114aad003743654c
vaishnaviajmire <124164730+vaishnaviajmire@users.noreply.github.com>
2023-03-31 20:10:03 +0530

    Add files via upload

---
+++ task1-Calculator/script.js
@@ -0,0 +1,31 @@
+let screen = document.getElementById("display");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+
+for(item of buttons){
+    item.addEventListener("click", (e) => {
+        buttonText = e.target.innerText;
+        if(buttonText == "x"){
+            buttonText = "*";
+            screenValue += buttonText;
+            screen.value = screenValue;
+        } 
+        else if(buttonText == "C"){
+            screenValue = "";
+            screen.value = screenValue;
+        } 
+        else if(buttonText == "="){
+            var result;
+            try{
+                result = eval(screenValue);
+            }catch(error){
+                result = "Expression error";
+            }
+            screen.value = result;
+        }
+        else{
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    });
+}
